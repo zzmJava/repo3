da8881fc67d683d3e0e4cf4656a1d058d3a2da45
zzm <715326196@qq.com>
2019-03-21 20:01:24 +0800

    分支完成新功能

---
+++ git_test/src/com/gitDemo.java
@@ -0,0 +1,13 @@
+package com;
+
+public class gitDemo {
+
+    public static void main(String[] args) {
+        String str = "你好啊";
+        test(2);
+    }
+
+    public static void test(int  i){
+        System.out.println((i & 1)==0);
+    }
+}
